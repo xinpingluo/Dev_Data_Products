99070433ed0a61aa7ba85c58574e634a9aa40c41
xinpingluo <xinping_luo@hotmail.com>
2015-05-23 16:39:23 +0200

    2nd

--- ui.R
+++ ui.R
@@ -8,11 +8,14 @@ shinyUI(pageWithSidebar(
     #,dateInput("date", "Date:")
   ),
   mainPanel(
-    p(strong('Your question is:')),textOutput("texto1"), p()
-    ,p(strong('Magic app says:')),textOutput("texto2")
-    ,p()
-    ,p(strong('Secret Formula: Magic Factor * Number of words * Questionmark Factor = Truth KPI:'))
-    ,p(textOutput("texto3"))
-                                    
+    tabsetPanel( 
+      tabPanel("Result", p(), p(strong('Your question is:')),textOutput("texto1"), p()
+      ,p(strong('Magic app says:')),textOutput("texto2")
+      ,p()
+      ,p(strong('Secret Formula: Magic Factor * Number of words * Questionmark Factor = Truth KPI:'))
+      ,p(textOutput("texto3"))
+      )
+      ,tabPanel("Documentation", p(), p('1. Enter your question'), p('2. The magic app delivers an answer'), p('The answer is calculated based on a randomized variable, the number of spaces and questionmarks of the question.'), p('The higher the number of spaces and questionmarks the higher the Truth KPI and the more likely the answer is going to be "Yes". '))
+    )                                
   )
 ))
